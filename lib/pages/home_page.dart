06e0b552dec818dd6244f67239ff7448a535a866
hoanganh2003 <tranhoanganh15022003@gmail.com>
2024-04-02 21:42:00 +0700

    sua back

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:dotted_line/dotted_line.dart';
+import 'package:flutter/services.dart';
 import 'package:testing/connection/conn.dart';
 import 'package:testing/pages/home_exercise_page.dart';
 import 'package:testing/pages/login_page.dart';
@@ -30,234 +31,260 @@ class _HomePageState extends State<HomePage> {
     double widthScreen = MediaQuery.of(context).size.width;
     double heightScreen = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-        key: _scaffoldKey,
-        drawer: NavBar(),
-        body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          width: widthScreen,
-          height: heightScreen,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/backgroundhome.jpg'),
-              fit: BoxFit.fill,
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 70),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: _openDrawer,
-                    icon: const Icon(Icons.menu),
-                    style: const ButtonStyle(
-                      iconSize: MaterialStatePropertyAll(34),
-                      iconColor: MaterialStatePropertyAll(Colors.white),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => UserPage(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                        ),
-                        borderRadius: BorderRadius.circular(6000),
-                      ),
-                      child: ClipOval(
-                        child: Image.asset(
-                          'images/japan.jpg',
-                          width: 40,
-                          height: 40,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async {
+        // Xác nhận với người dùng xem họ có muốn thoát ứng dụng hay không.
+        final confirmed = await showDialog<bool>(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Thoát ứng dụng'),
+            content: Text('Bạn có muốn thoát ứng dụng?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, false),
+                child: Text('Hủy'),
               ),
-              SizedBox(height: 60),
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+              TextButton(
+                onPressed: () => SystemNavigator.pop(),
+                child: Text('Thoát'),
+              ),
+            ],
+          ),
+        );
+
+        // Nếu người dùng xác nhận, trả về true để thoát ứng dụng.
+        return Future.value(confirmed);
+      },
+      child: Scaffold(
+          key: _scaffoldKey,
+          drawer: NavBar(),
+          body: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            width: widthScreen,
+            height: heightScreen,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/backgroundhome.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 70),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
-                          child: const Column(
-                            children: [
-                              Text(
-                                'START',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 30,
-                                ),
-                              ),
-                              Text(
-                                '03/09/2023',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.fromLTRB(0, 8, 20, 8),
-                          child: const Column(
-                            children: [
-                              Text(
-                                'END',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 30,
-                                ),
-                              ),
-                              Text(
-                                '02/09/2024',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                    IconButton(
+                      onPressed: _openDrawer,
+                      icon: const Icon(Icons.menu),
+                      style: const ButtonStyle(
+                        iconSize: MaterialStatePropertyAll(34),
+                        iconColor: MaterialStatePropertyAll(Colors.white),
+                      ),
                     ),
-                    SizedBox(height: 60),
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => UserPage(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.circular(6000),
+                        ),
+                        child: ClipOval(
+                          child: Image.asset(
+                            'images/japan.jpg',
+                            width: 40,
+                            height: 40,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 60),
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Container(
-                            height: 14,
-                            width: 14,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.white,
-                                width: 2,
-                              ),
-                              borderRadius: BorderRadius.circular(50),
-                            ),
-                          ),
-                          Expanded(
-                            child: DottedLine(
-                              dashLength: 10,
-                              dashGapLength: 8,
-                              lineThickness: 2,
-                              dashColor: Colors.white,
-                            ),
-                          ),
-                          RotatedBox(
-                            quarterTurns: 1,
-                            child: Icon(
-                              Icons.flight,
-                              color: Colors.white,
-                              size: 60,
-                            ),
-                          ),
-                          Expanded(
-                            child: DottedLine(
-                              dashLength: 10,
-                              dashGapLength: 8,
-                              lineThickness: 2,
-                              dashColor: Colors.white,
+                            padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
+                            child: const Column(
+                              children: [
+                                Text(
+                                  'START',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                  ),
+                                ),
+                                Text(
+                                  '03/09/2023',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                           Container(
-                            height: 14,
-                            width: 14,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(
-                                color: Colors.white,
-                                width: 2,
-                              ),
-                              borderRadius: BorderRadius.circular(50),
+                            margin: const EdgeInsets.fromLTRB(0, 8, 20, 8),
+                            child: const Column(
+                              children: [
+                                Text(
+                                  'END',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                  ),
+                                ),
+                                Text(
+                                  '02/09/2024',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '12:00 AM',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          '12:00 AM',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 60),
-                    Center(
-                      child: Text(
-                        'Còn 360 ngày 20 giờ 59 phút 59 giây',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
+                      SizedBox(height: 60),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 14,
+                              width: 14,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                            ),
+                            Expanded(
+                              child: DottedLine(
+                                dashLength: 10,
+                                dashGapLength: 8,
+                                lineThickness: 2,
+                                dashColor: Colors.white,
+                              ),
+                            ),
+                            RotatedBox(
+                              quarterTurns: 1,
+                              child: Icon(
+                                Icons.flight,
+                                color: Colors.white,
+                                size: 60,
+                              ),
+                            ),
+                            Expanded(
+                              child: DottedLine(
+                                dashLength: 10,
+                                dashGapLength: 8,
+                                lineThickness: 2,
+                                dashColor: Colors.white,
+                              ),
+                            ),
+                            Container(
+                              height: 14,
+                              width: 14,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                    SizedBox(height: 150),
-                    Column(
-                      children: [
-                        Text(
-                          'Học, học nữa, học mãi',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.w300,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '12:00 AM',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
                           ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          '- Lenin -',
+                          Text(
+                            '12:00 AM',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 60),
+                      Center(
+                        child: Text(
+                          'Còn 360 ngày 20 giờ 59 phút 59 giây',
                           style: TextStyle(
                             color: Colors.white,
                             fontSize: 18,
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w500,
                           ),
-                          textAlign: TextAlign.center,
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+                      ),
+                      SizedBox(height: 150),
+                      Column(
+                        children: [
+                          Text(
+                            'Học, học nữa, học mãi',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            '- Lenin -',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.italic,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
 
